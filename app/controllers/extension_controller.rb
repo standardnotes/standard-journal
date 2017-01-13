3856dadcf7708d42f0a16821f1bd837fc7e4fe2f
Mo Bitar <me@bitar.io>
2017-01-12 23:10:56 -0600

    params xml

--- app/controllers/extension_controller.rb
+++ app/controllers/extension_controller.rb
@@ -33,7 +33,9 @@ class ExtensionController < ApplicationController
     items.each do |item|
       post = Post.find_or_create_by(uuid: item[:uuid])
       post.title = item[:content][:title]
-      post.content = item[:content][:text]
+      content, params = content_and_params(item[:content][:text])
+      post.content = content
+      post.update(params)
       if post.path == nil
         post.path = post.title.parameterize
       end
@@ -43,4 +45,23 @@ class ExtensionController < ApplicationController
     render :json => {:success => true}
   end
 
+  def content_and_params(input)
+    if input.start_with? "<params>"
+      params_string = input.string_between_markers("<params>", "</params>")
+      if params_string
+        params_string = "<params>" + params_string + "</params>"
+        params = Hash.from_xml(params_string.gsub("\n", ""))
+        input.slice! params_string
+      end
+    end
+
+    return input, params["params"]
+  end
+
+end
+
+class String
+  def string_between_markers marker1, marker2
+    self[/#{Regexp.escape(marker1)}(.*?)#{Regexp.escape(marker2)}/m, 1]
+  end
 end
