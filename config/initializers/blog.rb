34253594dfdf1abe2616c449437ee4e82e8e019f
Mo Bitar <me@bitar.io>
2017-04-12 10:14:39 -0500

    config and style updates

--- config/initializers/blog.rb
+++ config/initializers/blog.rb
@@ -1,2 +1,4 @@
 template = ERB.new File.new("#{Rails.root}/config/blog.yml").read
 BLOG_CONFIG = YAML.load template.result(binding)
+BLOG_CONFIG["links"] = BLOG_CONFIG["links"] || []
+BLOG_CONFIG["hidden_posts"] = BLOG_CONFIG["hidden_posts"] || []
